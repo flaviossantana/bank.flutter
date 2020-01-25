fce4af3bbc3a4f97d49001d0fdfc46414f4f8aa0
Flávio Santana <9090flavio@gmail.com>
2020-01-24 22:09:52 -0300

    [FUNDAMENTOS DE FLUTTER][IMPLEMENTANDO LAYOUT COM WIDGETS][IMPLEMENTANDO O DESIGN DO ITEM DE TRANSFERÊNCIA]

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
         primaryColor: Colors.blueGrey,
       ),
       home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text("100,00"),
+                subtitle: Text("2032"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text("258,27"),
+                subtitle: Text("2032"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text("53,60"),
+                subtitle: Text("2032"),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: const Text(
             'Transferências',
@@ -18,7 +44,7 @@ void main() => runApp(MaterialApp(
           child: Icon(
             Icons.attach_money,
           ),
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.blueGrey,
         ),
       ),
       debugShowCheckedModeBanner: false,
