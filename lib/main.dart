a1e051579f14fad949070477aa6fa5e87a3163a6
Flávio Santana <9090flavio@gmail.com>
2020-01-24 23:04:00 -0300

    [FUNDAMENTOS DE FLUTTER][CRIANDO FORMULÁRIO E REFATORANDO O CÓDIGO][CONHECENDO O HOT RELOAD]

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.blueGrey,
-        ),
-        home: Scaffold(
-          body: Transferencias(),
-          appBar: AppBar(
-            title: const Text(
-              'Transferências',
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              //Vou adicionar algo aqui depois
-            },
-            child: Icon(
-              Icons.attach_money,
-            ),
-            backgroundColor: Colors.blueGrey,
-          ),
-        ),
-        debugShowCheckedModeBanner: false,
-      ),
+      FlutterBank(),
     );
+
+class FlutterBank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blueGrey,
+      ),
+      home: Scaffold(
+        body: TransferenciaForm(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class TransferenciaForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class Transferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(331.98, 2032)),
-        ItemTransferencia(Transferencia(432.54, 2032)),
-        ItemTransferencia(Transferencia(766.98, 2032)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Transferências',
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(331.98, 2032)),
+          ItemTransferencia(Transferencia(432.54, 2032)),
+          ItemTransferencia(Transferencia(323.98, 2032)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Vou adicionar algo aqui depois
+        },
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
