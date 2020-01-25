3e0bf04d04a43b43ebd3619c751a3b256a974601
Flávio Santana <9090flavio@gmail.com>
2020-01-24 22:36:45 -0300

    [FUNDAMENTOS DE FLUTTER][IMPLEMENTANDO LAYOUT COM WIDGETS][REFATORANDO O CÓDIGO]

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blueGrey,
-      ),
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("100,00"),
-                subtitle: Text("2032"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("258,27"),
-                subtitle: Text("2032"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("53,60"),
-                subtitle: Text("2032"),
-                trailing: Icon(Icons.more_vert),
-              ),
-            ),
-          ],
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blueGrey,
         ),
-        appBar: AppBar(
-          title: const Text(
-            'Transferências',
+        home: Scaffold(
+          body: Transferencias(),
+          appBar: AppBar(
+            title: const Text(
+              'Transferências',
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //Vou adicionar algo aqui depois
+            },
+            child: Icon(
+              Icons.attach_money,
+            ),
+            backgroundColor: Colors.blueGrey,
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //Vou adicionar algo aqui depois
-          },
-          child: Icon(
-            Icons.attach_money,
-          ),
-          backgroundColor: Colors.blueGrey,
-        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
-    ));
+    );
+
+class Transferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(331.98, 2032)),
+        ItemTransferencia(Transferencia(432.54, 2032)),
+        ItemTransferencia(Transferencia(766.98, 2032)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.conta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int conta;
+
+  Transferencia(this.valor, this.conta);
+}
