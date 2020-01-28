8da5344b7ed3b42f5c40a8a9779b6f68854ac44e
Flávio Santana <9090flavio@gmail.com>
2020-01-27 22:45:18 -0300

    [FUNDAMENTOS DE FLUTTER][CRIANDO FORMULÁRIO E REFATORANDO O CÓDIGO][ CRIANDO A TRANSFERÊNCIA]

--- lib/main.dart
+++ lib/main.dart
@@ -133,4 +133,11 @@ class Transferencia {
   final int conta;
 
   Transferencia(this.valor, this.conta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, conta: $conta}';
+  }
+
+
 }
