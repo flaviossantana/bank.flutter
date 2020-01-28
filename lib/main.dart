f06c754cf735bcd18b52067160d8e2c83fa5165e
Flávio Santana <9090flavio@gmail.com>
2020-01-28 20:01:11 -0300

    [FUNDAMENTOS DE FLUTTER][UTILIZANDO UMA LISTA DINÂMICA DE WIDGETS][EXTRAINDO WIDGETS FLEXÍVEIS]

--- lib/main.dart
+++ lib/main.dart
@@ -32,54 +32,65 @@ class TransferenciaForm extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _numeroContaTEC,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da Conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          TextFieldBnk(
+            controlador: _numeroContaTEC,
+            rotulo: 'Número da Conta:',
+            dica: '0000',
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _valorTEC,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0,00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          TextFieldBnk(
+              controlador: _valorTEC,
+              rotulo: 'Valor:',
+              dica: '0,00',
+              icone: Icons.monetization_on),
           RaisedButton(
             child: Text('Incluir'),
-            onPressed: () {
-              final int conta = int.tryParse(_numeroContaTEC.text);
-              final double valor = double.tryParse(_valorTEC.text);
-
-              if (isTransferenciaValida(conta, valor)) {
-                final novaTransferencia = Transferencia(valor, conta);
-                debugPrint('$novaTransferencia');
-              }
-            },
+            onPressed: () => _criarTransferencia(),
           ),
         ],
       ),
     );
   }
 
+  void _criarTransferencia() {
+    final int conta = int.tryParse(_numeroContaTEC.text);
+    final double valor = double.tryParse(_valorTEC.text);
+
+    if (isTransferenciaValida(conta, valor)) {
+      final novaTransferencia = Transferencia(valor, conta);
+      debugPrint('$novaTransferencia');
+    }
+  }
+
   bool isTransferenciaValida(int conta, double valor) =>
       conta != null && valor != null;
+}
+
+class TextFieldBnk extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  TextFieldBnk({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
 }
 
 class Transferencias extends StatelessWidget {
@@ -138,6 +149,4 @@ class Transferencia {
   String toString() {
     return 'Transferencia{valor: $valor, conta: $conta}';
   }
-
-
 }
