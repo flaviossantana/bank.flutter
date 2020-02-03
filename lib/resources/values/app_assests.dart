b9d5317d29c0737b72d385e6fdd51f671baff30a
Flávio Santana <9090flavio@gmail.com>
2020-02-03 19:42:16 -0300

    [PERSISTÊNCIA COM FLUTTER: CRIE UM APP COM ARMAZENAMENTO INTERNO][IMPLEMENTANDO O DASHBOARD][ADICIONANDO IMAGEM E BOTÃO DE CONTATOS]

--- lib/resources/values/app_assests.dart
+++ lib/resources/values/app_assests.dart
@@ -1,5 +1,16 @@
 class AppAssests {
 
-  static const String LOGO_BYTE_BNK = 'assets/logo/bytebank_logo.png';
+  static const String _ASSET_DIR = "assets";
+
+  //LOGOS
+  static const String _LOGO_DIR = "$_ASSET_DIR/logo";
+  static const String LOGO_BYTE_BNK = '$_LOGO_DIR/bytebank_logo.png';
+
+  //ICONS
+  static const String _ICON_DIR = "$_ASSET_DIR/icon";
+  static const String ICON_BOLSA_DINHEIRO = '$_ICON_DIR/bolsa-de-dinheiro.png';
+
+  //IMAGENS
+  static const String _IMAGENS_DIR = "$_ASSET_DIR/images";
 
 }
