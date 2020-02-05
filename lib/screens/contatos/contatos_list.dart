83f50971c35f3638e6878605d2894a86014902e8
Flávio Santana <9090flavio@gmail.com>
2020-02-04 23:45:37 -0300

    [PERSISTÊNCIA COM FLUTTER][CRIE UM APP COM ARMAZENAMENTO INTERNO][INTEGRANDO O BANCO DE DADOS COM AS TELAS][CARREGANDO A LISTA DE CONTATOS]

--- lib/screens/contatos/contatos_list.dart
+++ lib/screens/contatos/contatos_list.dart
@@ -6,16 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'contatos_form.dart';
 
-class Contatos extends StatefulWidget {
-  final List<Contato> _contatos = List();
-
-  @override
-  State<StatefulWidget> createState() {
-    return ContatosState();
-  }
-}
-
-class ContatosState extends State<Contatos> {
+class Contatos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,15 +15,22 @@ class ContatosState extends State<Contatos> {
           UIText.CONTATOS,
         ),
       ),
-      body: ListView.builder(
-          itemCount: widget._contatos.length,
-          itemBuilder: (context, index) {
-            final Contato contato = widget._contatos[index];
-            return ItemCardListView(
-              title: contato.nome,
-              subtitle: contato.conta.toString(),
-            );
-          }),
+      body: FutureBuilder(
+        future: todos(),
+        builder: (context, snapshot) {
+          final List<Contato> contatos = snapshot.data;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final Contato contato = contatos[index];
+              return ItemCardListView(
+                title: contato.nome,
+                subtitle: contato.conta.toString(),
+              );
+            },
+            itemCount: contatos.length,
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -54,14 +52,8 @@ class ContatosState extends State<Contatos> {
   }
 
   void _atualizar(Contato contato) {
-    setState(() {
-      if (contato != null) {
-        salvar(contato).then((id){
-          contato.id = id;
-          widget._contatos.add(contato);
-        });
-
-      }
-    });
+    if (contato != null) {
+      salvar(contato);
+    }
   }
 }
