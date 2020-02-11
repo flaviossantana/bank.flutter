947cb9f3a8a09bb51a5f08961d1b01cf09208382
Flávio Santana <9090flavio@gmail.com>
2020-02-11 20:40:22 -0300

    [FLUTTER COM WEB API: INTEGRANDO SUA APP MOBILE][MELHORANDO O WEBCLIENT][REFATORANDO O CÓDIGO]

--- lib/services/transacao_service.dart
+++ lib/services/transacao_service.dart
@@ -4,7 +4,7 @@ import 'package:fluterbank/models/transferencia.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-import 'loggin_interceptor.dart';
+import 'package:fluterbank/http/interceptors/loggin_interceptor.dart';
 
 const String _PATH = 'http://192.168.56.1:8080';
 const String _URL_TRANSACAO = '$_PATH/transactions';
@@ -20,30 +20,34 @@ class TransacaoService {
   Future<List<Transferencia>> todas() async {
     final Response response =
         await _client.get(_URL_TRANSACAO).timeout(Duration(seconds: 5));
+    return _paraTransacoes(response);
+    ;
+  }
 
+  List<Transferencia> _paraTransacoes(Response response) {
     final List<dynamic> decodeJson = jsonDecode(response.body);
-
     final List<Transferencia> transferencia = List();
-
     for (Map<String, dynamic> transferenciaMap in decodeJson) {
       transferencia.add(
         Transferencia.fromJson(transferenciaMap),
       );
     }
-
     return transferencia;
   }
 
   Future<Transferencia> salvar(Transferencia transferencia) async {
-    final Map<String, dynamic> transacaoMap = transferencia.toJson();
-    final String transacaoJson = jsonEncode(transacaoMap);
     final Response response = await _client.post(_URL_TRANSACAO,
         headers: {
           'Content-Type': 'application/json',
           'password': '1000',
         },
-        body: transacaoJson);
-
+        body: _paraTransacao(transferencia));
     return Transferencia.fromJson(jsonDecode(response.body));
+  }
+
+  String _paraTransacao(Transferencia transferencia) {
+    final Map<String, dynamic> transacaoMap = transferencia.toJson();
+    final String transacaoJson = jsonEncode(transacaoMap);
+    return transacaoJson;
   }
 }
